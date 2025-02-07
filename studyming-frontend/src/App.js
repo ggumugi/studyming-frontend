fae6500be5a5a3bc370c455dd5a44ea968b48ea0
ggumugi <phs970425@gmail.com>
2025-02-07 12:40:36 +0900

    hee design

--- studyming-frontend/src/App.js
+++ studyming-frontend/src/App.js
@@ -1,19 +1,19 @@
-import './App.css'
+import './styles/common.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import Header from './components/shared/Header'
 import Footer from './components/shared/Footer'
-import First from './components/pages/First'
+import WelcomePage from './pages/WelcomePage'
 // import Sidebar from './components/shared/Sidebar'
 
 function App() {
    return (
-      <BrowserRouter>
+      <>
          <Header />
-         <First />
+         <WelcomePage />
          {/* <Sidebar/> */}
          <Footer />
-      </BrowserRouter>
+      </>
    )
 }
 
