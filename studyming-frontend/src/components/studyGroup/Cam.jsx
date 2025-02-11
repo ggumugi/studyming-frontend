c90e880d9e9707b918854a6e0bc99017f4f4d852
000Lee <1003j6@gmail.com>
2025-02-11 17:59:16 +0900

    lee

---
+++ studyming-frontend/src/components/studyGroup/Cam.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Cam = () => {
+   return <div>📷 카메라 기능이 표시됩니다.</div>
+}
+
+export default Cam
---
+++ studyming-frontend/src/components/studyGroup/Chat.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Chat = () => {
+   return <div>📢 chat</div>
+}
+
+export default Chat
