b48d7fcd4acca4834ce7f997ee4ffd5fa8ea951d
ggumugi <phs970425@gmail.com>
2025-02-12 11:58:42 +0900

    pa

--- studyming-frontend/src/App.js
+++ studyming-frontend/src/App.js
@@ -22,6 +22,7 @@ import AdminPage from './pages/AdminPage'
 import StudyCreatePage from './pages/StudyCreatePage'
 import StudyDetailPage from './pages/StudyDetailPage'
 import StudyEditPage from './pages/StudyEditPage'
+import WelcomePage from './pages/WelcomePage'
 
 // import Sidebar from './components/shared/Sidebar'
 
@@ -49,6 +50,7 @@ function App() {
             <Route path="/study/create" element={<StudyCreatePage />} />
             <Route path="/study/detail/:id" element={<StudyDetailPage />} />
             <Route path="/study/edit/:id" element={<StudyEditPage />} />
+            <Route path="/welcome" element={<WelcomePage />} />
          </Routes>
          {/* <Sidebar/> */}
          <Footer />
