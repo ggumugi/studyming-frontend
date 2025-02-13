cbf83f59267c57151c29b8d79bff1cb70a9f832f
kangwonsik07 <alucard815@naver.com>
2025-02-13 16:42:42 +0900

    lee

---
+++ studyming-frontend/src/pages/StudyLeaderTransferPage.jsx
@@ -0,0 +1,13 @@
+// 스터디 생성
+import StudyLeaderTransfer from '../components/Study/StudyLeaderTransfer'
+//ui툴만 구현.
+
+const StudyLeaderTransferPage = () => {
+   return (
+      <>
+         <StudyLeaderTransfer />
+      </>
+   )
+}
+
+export default StudyLeaderTransferPage
