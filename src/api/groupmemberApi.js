6ad599dbae403dc7a3375b319cf3c6e470eab509
ggumugi <phs970425@gmail.com>
2025-02-25 16:15:30 +0900

    스터디 참여 완

--- src/api/groupmemberApi.js
+++ src/api/groupmemberApi.js
@@ -4,7 +4,18 @@ import studymingApi from './axiosApi'
 // 그룹 멤버 가입
 export const createGroupMember = async (groupId) => {
    try {
-      const response = await studymingApi.post(`/${groupId}`)
+      const response = await studymingApi.post(`/groupmember/${groupId}`)
+      return response
+   } catch (error) {
+      console.error(`API 오류: ${error.message}`)
+      throw error
+   }
+}
+
+// 그룹 멤버 상태 변경 (참여)
+export const participateInGroup = async (groupId) => {
+   try {
+      const response = await studymingApi.patch(`/groupmember/participate/${groupId}`) // PUT을 PATCH로 변경
       return response
    } catch (error) {
       console.error(`API 오류: ${error.message}`)
@@ -15,7 +26,7 @@ export const createGroupMember = async (groupId) => {
 // 그룹 멤버 탈퇴
 export const deleteGroupMember = async (groupId, userId) => {
    try {
-      const response = await studymingApi.delete(`/${groupId}/${userId}`)
+      const response = await studymingApi.delete(`/groupmember/${groupId}/${userId}`)
       return response
    } catch (error) {
       console.error(`API 오류: ${error.message}`)
@@ -26,7 +37,7 @@ export const deleteGroupMember = async (groupId, userId) => {
 // 그룹 멤버 정보 업데이트
 export const updateGroupMember = async (groupId, userId, updateData) => {
    try {
-      const response = await studymingApi.put(`/${groupId}/${userId}`, updateData)
+      const response = await studymingApi.put(`/groupmember/${groupId}/${userId}`, updateData)
       return response
    } catch (error) {
       console.error(`API 오류: ${error.message}`)
@@ -37,7 +48,7 @@ export const updateGroupMember = async (groupId, userId, updateData) => {
 // 그룹 멤버 전체 불러오기
 export const getGroupMembers = async (groupId) => {
    try {
-      const response = await studymingApi.get(`/${groupId}`)
+      const response = await studymingApi.get(`/groupmember/${groupId}`)
       return response
    } catch (error) {
       console.error(`API 오류: ${error.message}`)
@@ -48,7 +59,7 @@ export const getGroupMembers = async (groupId) => {
 // 그룹 멤버 특정인 불러오기
 export const getGroupMemberById = async (groupId, userId) => {
    try {
-      const response = await studymingApi.get(`/${groupId}/${userId}`)
+      const response = await studymingApi.get(`/groupmember/${groupId}/${userId}`)
       return response
    } catch (error) {
       console.error(`API 오류: ${error.message}`)
